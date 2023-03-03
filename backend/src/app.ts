a652016c1f75f782046237952013685fdaae2b91
hassanbaryal <h.baryal2@gmail.com>
2023-03-02 19:39:29 -0500

    Import and initialize passport

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,33 @@
 import express from 'express';
 import createError from 'http-errors';
 import path from 'path';
+import passport from 'passport';
 import logger from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
 import mongoose from 'mongoose';
 import session from 'express-session';
 import dotenv from 'dotenv';
+import initializePassport from './middlewares/passport-config.mw.js';
 import indexRouter from './routes/index.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || process.env.DEV_PORT;
 
+initializePassport(passport);
+
 app.use(compression());
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// DELETE LATER
+app.use((req, res, next) => {
+  console.log(req.body);
+  next();
+})
 
 app.use('/', indexRouter);
 
