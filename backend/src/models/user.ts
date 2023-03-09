a147ffacccfe369811aa5e9e6171981a3f00aca9
hassanbaryal <h.baryal2@gmail.com>
2023-03-08 20:24:47 -0500

    Create interface for user model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 
+export interface IUser {
+  username: string;
+  password: string;
+  timeStamp: Date;
+  admin: boolean;
+}
+
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema<IUser>({
   username: { type: String, max: 20, required: true },
   password: { type: String, required: true },
   timeStamp: { type: Date, default: Date.now(), required: true },
   admin: { type: Boolean, default: false, required: true },
 });
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model<IUser>('User', userSchema);
