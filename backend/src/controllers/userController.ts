9a9bea4de2ab51ddbefdc30c014bd852b589c3f6
hassanbaryal <h.baryal2@gmail.com>
2023-03-08 20:23:49 -0500

    Create function to handle login on post. Fix some errors and bugs in signup function

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
+import { Request, Response } from 'express';
 
 // Create user on POST
 const signup_post = [
@@ -19,9 +20,8 @@ const signup_post = [
     .isLength({ min: 1 })
     .escape()
     .withMessage('Password is required'),
-  (req: any, res: any) => {
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
-    // console.log(req.body);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
@@ -35,14 +35,14 @@ const signup_post = [
         });
 
       const user = new User({
-        username: 'test',
+        username: req.body.username,
         password: hashedPassword,
       });
 
-      return user.save((err) => {
+      return user.save((err: unknown) => {
         if (err)
           return res.status(500).json({
-            msg: 'Something went wrong',
+            message: 'Something went wrong',
           });
 
         return res.status(201).json({
@@ -53,4 +53,60 @@ const signup_post = [
   },
 ];
 
-export { signup_post };
+// Login user on POST
+const login_post = [
+  body('username')
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .isAlphanumeric()
+    .escape()
+    .withMessage(
+      'Username is required (max 20 characters, letters and numbers only'
+    ),
+  body('password')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Password is required'),
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    passport.authenticate(
+      'local',
+      { session: false },
+      (err: Error, user: any, info: unknown) => {
+        // If error or no user found
+        if (err || !user) {
+          return res.status(400).json(info);
+        }
+
+        req.login(user, { session: false }, (error: Error) => {
+          if (error)
+            return res.status(500).json({
+              message: 'Something went wrong',
+            });
+          
+          const jwtSecret = process.env.JWT_SECRET || 'jwtsecret';
+
+          const token = jwt.sign(
+            user.toJSON(),
+            jwtSecret,
+            {
+              expiresIn: '1d'
+            }
+          );
+          return res.status(200).json({ user, token: `Bearer ` + token });
+        });
+      }
+    )(req, res);
+  },
+];
+
+
+export { signup_post, login_post };
