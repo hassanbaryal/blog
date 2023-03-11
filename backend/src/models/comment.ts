2423271cc7e46fe9e1ca722c109048cc5e8c5ee5
hassanbaryal <h.baryal2@gmail.com>
2023-03-10 19:05:24 -0500

    Create model schema for comments

---
+++ backend/src/models/comment.ts
@@ -0,0 +1,27 @@
+import mongoose, { ObjectId } from 'mongoose';
+import { DateTime } from 'luxon';
+const { Schema } = mongoose;
+
+export interface IComment {
+  text: string;
+  timeStamp: Date;
+  user: ObjectId;
+  post: ObjectId;
+  likes: ObjectId[];
+}
+
+const commentSchema = new Schema<IComment>({
+  text: { type: String, max: 300, required: true },
+  timeStamp: { type: Date, default: Date.now(), required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  post: { type: Schema.Types.ObjectId, ref: 'Post' },
+  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+});
+
+commentSchema.virtual('formattedTimeStamp').get(function () {
+  return DateTime.fromJSDate(this.timeStamp).toLocaleString(
+    DateTime.DATETIME_MED
+  );
+});
+
+export default mongoose.model<IComment>('Comment', commentSchema);
