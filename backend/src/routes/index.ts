ef98b46cde2d8a52a21a18ebb7228575cba5f7be
hassanbaryal <h.baryal2@gmail.com>
2023-03-15 18:56:24 -0400

    Add routes to handle admin requests

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as userController from '../controllers/userController.js';
+import * as adminController from '../controllers/adminController.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -12,7 +13,13 @@ router.get('/', (req, res) => {
 // POST create new user on signup
 router.post('/signup', userController.signup_post);
 
+// POST create new admin on signup
+router.post('/admin/signup', adminController.createAdminUser_post);
+
 // POST log user in
 router.post('/login', userController.login_post);
+
+// POST log admin in
+router.post('/admin/login', adminController.adminLogin_post);
 
 export default router;
